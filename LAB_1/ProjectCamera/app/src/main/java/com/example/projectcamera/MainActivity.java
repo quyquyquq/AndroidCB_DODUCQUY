8f272965537a6cd799bdc991e1425aa53907cdd8
DO DUC QUY <quy2642004@gmail.com>
2024-08-31 01:06:59 +0700

    a

---
+++ LAB_1/ProjectCamera/app/src/main/java/com/example/projectcamera/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.projectcamera;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_IMAGE_CAPTURE = 100;
+    private ImageView imgPhoto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Khởi tạo các điều khiển
+        innitControl();
+    }
+
+    private void innitControl() {
+        Button btnCamera = findViewById(R.id.btnCamera);
+        imgPhoto = findViewById(R.id.imgPhoto);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK && data != null) {
+            Bundle extras = data.getExtras();
+            if (extras != null) {
+                Bitmap bitmap = (Bitmap) extras.get("data");
+                if (bitmap != null) {
+                    imgPhoto.setImageBitmap(bitmap);
+                }
+            }
+        }
+    }
+}
